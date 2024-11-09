7c09259bb961384f6425e90c2b1ce4dc66683169
ftreff <ftreff@gmail.com>
2024-11-09 00:15:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,110 @@
 #!/bin/bash
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣶⡎⠉⠀⠙⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠿⠉⠀⠀⠀⠀⠀⠈⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀ "
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⣿⠛⠶⠤⠀⠀⠀⠀⠀⠀⠀⠀⠈⣷⡀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣥⣈⠉⠒⠦⣄⠀⣀⠀⠀⠀⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠛⠓⠲⣄⠈⠳⡌⠳⡀⠀⠀⠀⢸⣷⡇⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⡇⠀⠀⠈⠳⡀⠈⢦⡹⡀⠀⠀⢸⠃⢧⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠟⢳⣤⠀⢻⡿⣆⠀⢳⡗⠀⠀⡼⠀⢸⡆⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣷⣤⡟⠀⠀⠈⠛⣆⠀⢷⠀⠀⡇⠀⠨⢧⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣧⣠⠀⠀⠀⠘⣆⠈⠃⣰⠁⠀⠄⠸⣦⡀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣷⡄⠀⠀⠀⠸⡅⢀⡏⠀⠀⠀⢠⠏⠱⣄⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣷⣤⣠⠖⢻⠁⡼⠀⠀⢀⡴⠋⠀⠀⠈⢦⡀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡟⠉⢻⡻⣿⣿⣿⢧⣠⢏⣾⣡⠤⠚⣏⠀⠀⠀⠀⠀⠀⠉⠣⡄⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⡿⠁⢠⢿⣿⢿⣿⡿⠋⣿⡏⠉⠀⠀⠀⣹⡞⠁⠀⠀⠀⠀⠀⠀⢸⡀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⣆⡴⡟⢸⢸⢰⡄⠀⠀⣹⢱⠀⠀⠀⢰⢿⡄⠀⠀⠀⠀⠀⠀⠀⠀⢧"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⠃⣿⠀⠃⢸⢸⠘⡇⠀⠀⣿⢸⠀⠀⠀⠃⠀⢧⡄⢀⡴⠃⠀⠀⠀⠀⠘"
+echo "⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⢿⡧⣿⠀⠀⡸⣾⠀⡇⠀⠀⣯⡏⠀⠀⠀⠀⠀⣸⡷⣫⣴⠀⠀⠀⢀⠂⢀"
+echo "⠘⣿⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣇⠀⠀⣿⠀⠀⡇⣿⠰⠇⠀⣸⢻⠇⠀⠀⠀⠀⢰⠿⠞⣫⢞⡠⠀⢀⠂⠀⢸"
+echo "⠀⠘⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⣏⠻⣦⣤⣿⠀⠀⢧⡇⠀⠀⠀⢹⣾⠀⠀⠀⠀⢠⡏⣠⣼⣋⣉⣀⣴⣁⣀⣀⡎"
+echo "⠀⠀⠈⢿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣷⡌⠙⠺⢭⡿⠀⠀⠸⠆⠀⠀⠀⢸⣿⡀⠀⠀⠀⡟⢀⡧⣄⣠⣠⣤⣤⣤⣀⣈⡇"
+echo "⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠿⠃⠀⠈⠢⠐⢤⣧⠀⠀⠀⠀⠀⠀⠀⢸⡿⠀⠀⠀⣼⠁⡼⠉⠛⠒⠒⠒⠒⠶⠶⢿⠁"
+echo "⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⢀⣤⣛⡛⠛⢢⠀⠀⢠⠈⢪⣻⡇⠀⠀⠀⠀⠀⠀⠐⠃⠀⠀⢰⠏⢸⡧⠤⠤⠤⢤⣀⣀⡀⠀⡾⠀"
+echo "⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⣀⣀⠤⠴⠒⠚⣩⠽⣿⠖⠋⠉⠀⠀⣦⠈⣧⠀⠈⣳⣼⡿⠛⠀⠀⠀⠀⠀⠀⠀⢀⡤⠴⠞⠀⣿⠓⠢⠤⠤⠤⠤⣌⣉⣻⡇⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣭⣭⣶⣦⣤⣶⠋⢡⣴⠇⢀⣴⡦⠀⣠⢿⣤⣿⡴⠒⢹⣏⣀⠀⠀⢀⣀⣀⠀⠀⢀⣠⣄⢀⣤⣾⡯⡀⠀⠉⠒⠒⠤⢤⣭⣽⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢠⣻⠃⡴⠛⢁⣴⡯⠇⠀⠀⠈⠉⠉⠉⢹⡍⠉⠉⠙⣷⠈⢻⠉⠻⠀⠘⣟⠻⠀⡉⠁⠀⠀⠀⠀⠀⠀⣠⣿⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣮⣵⢰⣧⣞⣶⡿⢋⣡⠔⠚⣀⡀⠀⠀⠀⠀⢨⠇⠀⠀⠀⢹⠀⠈⠁⠀⠀⠀⠿⠀⠀⠈⠓⠶⠄⠀⠐⣲⡾⠋⡿⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣾⡿⢿⣿⢎⢠⠟⡠⣾⠟⢋⡠⠤⠤⢤⠤⠾⠤⠤⣤⢤⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠞⠁⢀⣴⠇⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⡙⠻⣿⣿⣿⣿⣝⡋⣮⣴⣞⣥⡄⠀⠀⢀⣀⡤⠴⠚⠛⠪⣟⡧⢤⣄⣠⣄⡐⠦⣤⣤⣤⠴⠚⠉⠀⠀⠀⣾⠁⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⡄⠈⠙⢿⣿⣿⣿⣿⠟⠋⣁⣤⠴⠚⠉⠁⠀⠀⠀⠀⠀⠀⠉⠲⢤⡀⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⢀⣿⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣿⡄⠀⠀⢙⣹⣷⠶⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠦⣄⠀⠀⠀⠀⠀⠀⠀⠰⢚⡇⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⡾⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠂⠀⠀⠀⠀⠀⠈⠛⠃⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo ""
+sleep 2
+echo ""
+echo ""
+echo "              ·▄▄▄ ▄▄▄·▄▄▄▄▄"
+echo "              ▐▄▄·▐█ ▀█•██"
+echo "              ██▪ ▄█▀▀█ ▐█.▪"
+echo "              ██▌.▐█ ▪▐▌▐█▌·"
+echo "              ▀▀▀  ▀  ▀ ▀▀▀"
+echo "      ▄▄▌ ▐ ▄▌ ▄▄▄· ▄▄▄  ▄▄▄ .·▄▄▄▄•"
+echo "      ██· █▌▐█▐█ ▀█ ▀▄ █·▀▄.▀·▪▀·.█▌"
+echo "      ██▪▐█▐▐▌▄█▀▀█ ▐▀▀▄ ▐▀▀▪▄▄█▀▀▀•"
+echo "      ▐█▌██▐█▌▐█ ▪▐▌▐█•█▌▐█▄▄▌█▌▪▄█▀"
+echo "       ▀▀▀▀ ▀▪ ▀  ▀ .▀  ▀ ▀▀▀ ·▀▀▀ •"
+echo "Common packages & applications install script"
+echo "        Download / Install / Build"
+echo ""
+echo ""
+sleep 1
 echo "This scrip will download, then build and/or install various applications."
-echo "The list of application is in the readme of the repository"
-
+echo ""
+sleep 1
+echo "Current list of applications:"
+sleep 1
+echo "Git"
+sleep .1
+echo "TraceRoute"
+sleep .1
+echo "GoTop"
+sleep .1
+echo "NeoFetch"
+sleep .1
+echo "Screen"
+sleep .1
+echo "LibSensors"
+sleep .1
+echo "Htop"
+sleep .1
+echo "Docker"
+sleep .1
+echo "Gimp"
+sleep .1
+echo "XMRig"
+sleep .1
+echo "XMRig-MO (MoneroOcean Fork)"
+echo ""
+sleep 1
+echo "Most apps will install once you press 'y', however a few will ask again if you want to install them."
+echo "The apps you have the option to skip are;"
+echo "Docker - if you dont plan on using docker, there is no need to install it."
+echo "Gimp - this is a GUI application, if your running CLI only, there is no reason to install this"
+echo "XMRig and XMRig-MO - these take a few minutes to build, if you dont plan on mining monero, there is no need to download and build them."
 read -p "Do you want to proceed (y/n) " yn
 
 case $yn in 
-	y ) echo "ok, Installing apps...";;
-	n ) echo "ok, exiting...";
+	y ) echo "You pressed 'y', Installing apps...";;
+	n ) echo "You pressed 'n', exiting...";
 		sleep 1
 		exit;;
-	* ) echo "invalid response, exiting";
+	* ) echo "You pressed an invalid response, exiting...";
 		sleep 1
 		exit 1;;
 esac
-
+sleep 1
 cd
 #-----UPDATES-----
 echo "Downloading apt updates..."
 yes | sudo apt update
 echo "Preforming apt full upgrade..."
 yes | sudo apt full-upgrade
+#-----git-----
+echo "Installing Git..."
+yes | sudo apt install git
 #-----traceroute-----
 echo "Installing TraceRoute..."
 yes | sudo apt install traceroute
@@ -29,9 +114,6 @@ cd /tmp
 wget https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_arm64.deb
 yes | sudo apt install /tmp/gotop_v4.2.0_linux_arm64.deb
 rm gotop_v4.2.0_linux_arm64.deb
-#-----Docker-----
-echo "Installing Docker..."
-yes | sudo apt install docker.io
 #-----neofetch-----
 echo "Installing NeoFetch..."
 yes | sudo apt install neofetch
@@ -41,41 +123,82 @@ yes | sudo apt install screen
 #-----libsensors5-----
 echo "Installing LibSensors5..."
 yes | sudo apt install libsensors5
+#-----Docker-----
+echo "Do you want to install Docker? (y/n)"
+read -r choice
+
+if [ "$choice" = "y" ]; then
+    echo "Installing Docker..."
+    yes | sudo apt install docker.io
+else
+    echo "Docker will only install if 'y' was pressed."
+    echo "You did NOT press 'y'.  Skipping Gimp..."
+    sleep 1
+fi
+#-----gimp-----
+echo "Do you want to install Gimp? (y/n)"
+read -r choice
+
+if [ "$choice" = "y" ]; then
+    echo "Installing Gimp..."
+    yes | sudo apt install gimp
+else
+    echo "Gimp will only install if 'y' was pressed."
+    echo "You did NOT press 'y'.  Skipping Gimp..."
+    sleep 1
+fi
+
 #-----xmrig & xmrig-mo-----
-echo "Do you want to download and build XMRIG & XMRIG-MO?"
+echo "Do you want to download and build XMRIG & XMRIG-MO? (y/n)"
+read -r choice
 
-read -p "Press 'y' to continue or 'n' to exit (y/n) " yn
+if [ "$choice" = "y" ]; then
+    echo "You pressed 'y', Downloading and Building XMRIG & XMRIG-MO..."
+    cd
+    echo "Downloading xmrig..."
+    sleep .2
+    git clone https://github.com/xmrig/xmrig.git
+    sleep .2
+    echo "Downloading xmrig-mo..."
+    sleep .2
+    cd
+    git clone https://github.com/MoneroOcean/xmrig.git xmrig-mo
+    sleep .2
+    echo "Downloading needed packages to proceed..."
+    sleep .2
+    yes | sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+    sleep .2
+    echo "Building xmrig..."
+    sleep .2
+    cd && cd xmrig && mkdir build && cd build && cmake .. && sudo make
+    sleep .2
+    echo "Building xmrig-mo..."
+    sleep .2
+    cd && cd xmrig-mo && mkdir build && cd build && cmake .. && sudo make
+    sleep .2
+    echo "xmrig and xmrig-mo are now ready for use in your home directory..."
+    sleep 1
 
-case $yn in 
-	y ) echo "ok, Installing XMRIG & XMRIG-MO...";;
-	n ) echo "ok, exiting...";
-		sleep 1
-		exit;;
-	* ) echo "invalid response, exiting";
-		sleep 1
-		exit 1;;
-esac
-cd
-echo "Downloading xmrig..."
-sleep .2
-git clone https://github.com/xmrig/xmrig.git
-sleep .2
-echo "Downloading xmrig-mo..."
-sleep .2
-cd
-git clone https://github.com/MoneroOcean/xmrig.git xmrig-mo
-sleep .2
-echo "Downloading needed packages to proceed..."
-sleep .2
-yes | sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-sleep .2
-echo "Building xmrig..."
-sleep .2
-cd && cd xmrig && mkdir build && cd build && cmake .. && sudo make
-sleep .2
-echo "Building xmrig-mo..."
-sleep .2
-cd && cd xmrig-mo && mkdir build && cd build && cmake .. && sudo make
-sleep .2
-echo "xmrig and xmrig-mo are now ready for use in your home directory..."
+else
+    echo "XMRig and XMRig-MO will only install if 'y' was pressed."
+    echo "You did NOT press 'y'.  Skipping XMRig and XMRig-MO..."
+    sleep 1
+fi
+#Script is completed, now exiting
 sleep 1
+echo "    ▪   ▐ ▄ .▄▄ · ▄▄▄▄▄ ▄▄▄· ▄▄▌  ▄▄▌        "
+echo "    ██ •█▌▐█▐█ ▀. •██  ▐█ ▀█ ██•  ██•        "
+echo "    ▐█·▐█▐▐▌▄▀▀▀█▄ ▐█.▪▄█▀▀█ ██▪  ██▪        "
+echo "    ▐█▌██▐█▌▐█▄▪▐█ ▐█▌·▐█ ▪▐▌▐█▌▐▌▐█▌▐▌      "
+echo "    ▀▀▀▀▀ █▪ ▀▀▀▀  ▀▀▀  ▀  ▀ .▀▀▀ .▀▀▀       "
+echo " ▄▄·       • ▌ ▄ ·.  ▄▄▄·▄▄▌  ▄▄▄ .▄▄▄▄▄▄▄▄ ."
+echo "▐█ ▌▪▪     ·██ ▐███▪▐█ ▄███•  ▀▄.▀·•██  ▀▄.▀·"
+echo "██ ▄▄ ▄█▀▄ ▐█ ▌▐▌▐█· ██▀·██▪  ▐▀▀▪▄ ▐█.▪▐▀▀▪▄"
+echo "▐███▌▐█▌.▐▌██ ██▌▐█▌▐█▪·•▐█▌▐▌▐█▄▄▌ ▐█▌·▐█▄▄▌"
+echo "·▀▀▀  ▀█▄▀▪▀▀  █▪▀▀▀.▀   .▀▀▀  ▀▀▀  ▀▀▀  ▀▀▀ "
+echo ""
+sleep 1
+echo "Script is complete, now exiting..."
+sleep 2
+exit 0
+
